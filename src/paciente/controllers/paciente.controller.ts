f96b55143ab081c9562cf0064ca60d4056eec96f
adryanlops <adylopesk8@gmail.com>
2026-02-01 12:47:58 -0300

    fix paciente controller

--- src/paciente/controllers/paciente.controller.ts
+++ src/paciente/controllers/paciente.controller.ts
@@ -9,7 +9,6 @@ import {
   ParseIntPipe,
   Post,
   Put,
-  Req,
   UseGuards,
 } from '@nestjs/common';
 import { Paciente } from '../entities/paciente.entity';
@@ -17,9 +16,7 @@ import { DeleteResult } from 'typeorm';
 import { PacienteService } from '../services/paciente.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
-import { Roles } from '../../usuario/decorator/roles.decorator';
-import { UsuarioTipo } from '../../usuario/entities/usuario.entity';
-import { RolesGuard } from '../../auth/guard/roles.guard';
+
 
 @ApiTags('Pacientes')
 @UseGuards(JwtAuthGuard)
@@ -28,12 +25,14 @@ import { RolesGuard } from '../../auth/guard/roles.guard';
 export class PacienteController {
   constructor(private readonly pacienteService: PacienteService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get() // Indica qual tipo de Requisição esse método é executado
   @HttpCode(HttpStatus.OK) // monta a Resposta HTTP para o Front com o status 200
   findAll(): Promise<Paciente[]> {
     return this.pacienteService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/:id') // sai de id = "1" --> id = 1
   @HttpCode(HttpStatus.OK) // monta a Resposta HTTP para o Front com o status 200
   findById(@Param('id', ParseIntPipe) id: number): Promise<Paciente> {
@@ -41,6 +40,7 @@ export class PacienteController {
     return this.pacienteService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/nome/:nome') // pacientes/nome/{texto}
   @HttpCode(HttpStatus.OK)
   findAllByName(@Param('nome') nome: string): Promise<Paciente[]> {
@@ -55,12 +55,14 @@ export class PacienteController {
     return this.pacienteService.create(paciente);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put()
   @HttpCode(HttpStatus.OK)
   update(@Body() paciente: Paciente): Promise<Paciente> {
     return this.pacienteService.update(paciente);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
